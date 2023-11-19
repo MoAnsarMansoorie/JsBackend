2f1b67c505aae019161e918c2a73419e0d28fd3e
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2023-11-19 17:21:35 +0530

    asyncHandler

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@
 
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
 connectDb()
+.then(() => {
+    app.on("error", (err) => {
+        console.log(`Some error in listening of PORT`);
+        throw err
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection failed!!!!!: ", err);
+})
 
 
 
